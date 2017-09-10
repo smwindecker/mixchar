17c7f8a340c64a5245362d141d4a5f4486c1a059
Saras Windecker <windeckers@student.unimelb.edu.au>
2017-09-10 17:21:27 +1000

    revised DESCRIPTION and fixed deconvolve.R file

--- R/deconvolve.R
+++ R/deconvolve.R
@@ -1,20 +1,22 @@
-#' Optimise Parameters
+#' Deconvolves Thermogravimetric Data
 #'
-#' This function otimises parameter selection so that the model will ultimately converge.
+#' This function deconvolves thermogravimetric data using a three-part
+#' Fraser-Suzuki mixture model
 #'
 #' @param data vector of starting values for each of 12 parameters
 #' @param temp temperature values
 #' @param obs DTG mass values
 #' @return model output
 #' @keywords thermogravimetry fraser-suzuki deconvolution
-#' @export deconvolve()
+
+#' @export
 
 deconvolve <- function (data, temp, obs) {
 
   theta <- c(.015, .013, .01, -.15, -.15, -.15, 540, 600, 700, 50, 30, 200)
   lb <- c(0, 0, 0, -1, -1, -1, 0, 0, 0, 0, 0, 0)
-  params_opt <- .paramSelect(theta, lb, .fsTotal, temp, obs, restarts = 300)
-  fs <- .fsModel(data, params_opt)
+  params_opt <- .param_select(theta, lb, .fsTotal, temp, obs, restarts = 300)
+  fs <- .fs_model(data, params_opt)
 
   return(fs)
 
