f951ad8212235ff150dbe1e314d72f0077d01f6d
Saras Mei Windecker <windeckers@6300D-99394-M.local>
2017-10-04 12:28:53 +1100

    change example dataset for 4 curves

---
+++ R/marsilea-data.R
@@ -0,0 +1,17 @@
+#' Thermogravimetric data for Marsilea drumondii
+#'
+#' Raw thermogravimetric data from the wetland forb, M. drumondii.
+#'
+#' @docType data
+#'
+#' @usage data(marsilea)
+#'
+#' @format An object of class \code{'cross'}
+#'
+#' @keywords datasets
+#'
+#' @source Saras M Windecker
+#' @examples
+#' data(marsilea)
+
+'marsilea'
