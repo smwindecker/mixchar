a9feec251d30f09fb7564abcd762f539791963cb
Saras Mei Windecker <windeckers@6300D-99394-M.local>
2017-10-04 12:06:54 +1100

    add additional example dataset with four curves

---
+++ R/cyperus-data.R
@@ -0,0 +1,17 @@
+#' Thermogravimetric data for Cyperus eragrostis
+#'
+#' Raw thermogravimetric data from the wetland sedge, C. eragrostis.
+#'
+#' @docType data
+#'
+#' @usage data(cyperus)
+#'
+#' @format An object of class \code{'cross'}
+#'
+#' @keywords datasets
+#'
+#' @source Saras M Windecker
+#' @examples
+#' data(cyperus)
+
+'cyperus'
