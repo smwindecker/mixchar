dc5d5c2885e02bdbcd5f1052852ed983b97ee5ca
Saras Windecker <windeckers@student.unimelb.edu.au>
2017-10-12 16:28:56 +1100

    add lower bounds to model

--- R/fs_model.R
+++ R/fs_model.R
@@ -10,7 +10,7 @@
 # @import minpack.lm
 
 # function to do the nls fit with the correct starting values
-fs_model <- function (dataframe, params) {
+fs_model <- function (dataframe, params, lb) {
 
   nlsLM(deriv ~ fs_mixture_wrap(temp_K, h1, h2, h3, s1, s2, s3, p1, p2, p3, w1, w2, w3),
         start = list(h1 = params[1], h2 = params[2], h3 = params[3],
@@ -18,6 +18,7 @@ fs_model <- function (dataframe, params) {
                      p1 = params[7], p2 = params[8], p3 = params[9],
                      w1 = params[10], w2 = params[11], w3 = params[12]),
         data = dataframe,
-        control = nls.lm.control(maxiter = 1024, maxfev = 1e6))
+        control = nls.lm.control(maxiter = 1024, maxfev = 1e6),
+        lower = lb)
 
 }
--- R/fs_model_4.R
+++ R/fs_model_4.R
@@ -10,7 +10,7 @@
 # @import minpack.lm
 
 # function to do the nls fit with the correct starting values
-fs_model_4 <- function (dataframe, params) {
+fs_model_4 <- function (dataframe, params, lb) {
 
   nlsLM(deriv ~ fs_mixture_wrap_4(temp_K, h1, h2, h3, h4, s1, s2, s3, s4, p1, p2, p3, p4, w1, w2, w3, w4),
         start = list(h1 = params[1], h2 = params[2], h3 = params[3], h4 = params[4],
@@ -18,6 +18,7 @@ fs_model_4 <- function (dataframe, params) {
                      p1 = params[9], p2 = params[10], p3 = params[11], p4 = params[12],
                      w1 = params[13], w2 = params[14], w3 = params[15], w4 = params[16]),
         data = dataframe,
-        control = nls.lm.control(maxiter = 1024, maxfev = 1e6))
+        control = nls.lm.control(maxiter = 1024, maxfev = 1e6),
+        lower = lb)
 
 }
