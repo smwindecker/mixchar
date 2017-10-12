e444c63f9c6c75a3ffee61c47c4b973ff580e26a
Saras Windecker <windeckers@student.unimelb.edu.au>
2017-10-12 16:30:01 +1100

    revise deconvolve and add function to detect peaks

---
+++ R/three_peaks.R
@@ -0,0 +1,8 @@
+# Function to convert inflection point to logical argument for presence of fourth peak
+#
+# @param x numeric
+# @keywords internal
+
+three_peaks <- function(x) {
+  return(length(x) == 0)
+}
