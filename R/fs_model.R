433b003910c2689402b077e674d130ed5cfb0ef2
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-13 17:18:41 +1100

    merge the model functions into one and document

--- R/fs_model.R
+++ R/fs_model.R
@@ -1,22 +1,36 @@
-# Non-linear model using Fraser-Suzuki mixture model
-#
-# Non-linear model output using optimised parameter values
-# with a three-part mixture model using Fraser-Suzuki equation
-#
-# @param dataframe dataframe
-# @param params starting parameter values
-# @return model output
-# @keywords internal
-# @import minpack.lm
+#' Non-linear model using Fraser-Suzuki mixture model
+#'
+#' Non-linear model output using optimised parameter values
+#' with a three-part mixture model using Fraser-Suzuki equation
+#'
+#' @param dataframe dataframe
+#' @param params starting parameter values
+#' @param lb lower bounds for model
+#' @param ub upper bounds for model
+#' @importFrom minpack.lm nlsLM
+#' @return model output
+#'
+#' @export
 
-# function to do the nls fit with the correct starting values
 fs_model <- function (dataframe, params, lb, ub) {
 
-  nlsLM(deriv ~ fs_mixture_wrap(temp_C, h1, h2, h3, s1, s2, s3, p1, p2, p3, w1, w2, w3),
-        start = list(h1 = params[1], h2 = params[2], h3 = params[3],
-                     s1 = params[4], s2 = params[5], s3 = params[6],
-                     p1 = params[7], p2 = params[8], p3 = params[9],
-                     w1 = params[10], w2 = params[11], w3 = params[12]),
+  if (length(params) == 12) {
+    pred <- fs_mixture(temp_C, h1, s1, p1, w1, h2, s2, p2, w2, h3, s3, p3, w3)
+    start_list <- list(h1 = params[1], s1 = params[2], p1 = params[3], w1 = params[4],
+                       h2 = params[5], s2 = params[6], p2 = params[7], w2 = params[8],
+                       h3 = params[9], s3 = params[10], p3 = params[11], w3 = params[12])
+  }
+
+  if (length(params) == 16) {
+    pred <- fs_mixture(temp_C, h1, s1, p1, w1, h2, s2, p2, w2, h3, s3, p3, w3, h4, s4, p4, w4)
+    start_list <- list(h1 = params[1], s1 = params[2], p1 = params[3], w1 = params[4],
+                       h2 = params[5], s2 = params[6], p2 = params[7], w2 = params[8],
+                       h3 = params[9], s3 = params[10], p3 = params[11], w3 = params[12],
+                       h4 = params[13], s4 = params[14], p4 = params[15], w4 = params[16])
+  }
+
+  nlsLM(deriv ~ pred,
+        start = start_list,
         data = dataframe,
         control = nls.lm.control(maxiter = 1024, maxfev = 1e6),
         lower = lb,
