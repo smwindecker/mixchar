be0ce15c6033d14a1ca3eea7976b5d703d5fbce9
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-13 17:18:13 +1100

    merge all the mixture functions into one and document

--- R/fs_mixture.R
+++ R/fs_mixture.R
@@ -1,17 +1,38 @@
-# Fraser-Suzuki three-part mixture
-#
-# This function combines three Fraser-Suzuki functions for the total mixture model.
-#
-# @param x temperature values
-# @param params parameter starting values
-# @return Fraser-Suzuki mixture model output
-# @keywords internal
+#' Mixture model for TGA data
+#'
+#' @param x temperature values
+#' @param h1 height value for curve 1
+#' @param s1 shape value for curve 1
+#' @param p1 position value for curve 1
+#' @param w1 width value for curve 1
+#' @param h2 height value for curve 2
+#' @param s2 shape value for curve 2
+#' @param p2 position value for curve 2
+#' @param w2 width value for curve 2
+#' @param h3 height value for curve 3
+#' @param s3 shape value for curve 3
+#' @param p3 position value for curve 3
+#' @param w3 width value for curve 3
+#' @param h4 height value for curve 4, if present
+#' @param s4 shape value for curve 4, if present
+#' @param p4 position value for curve 4, if present
+#' @param w4 width value for curve 4, if present
+#' @return Fraser-Suzuki model output
+#'
+#' @export
 
-# combine the FS function three times for the three pseudo-components
-fs_mixture <- function (x, params) {
+fs_mixture <- function (x, h1, s1, p1, w1,
+                        h2, s2, p2, w2,
+                        h3, s3, p3, w3,
+                        h4 = NULL, s4 = NULL, p4 = NULL, w4 = NULL) {
 
-  fs_mixture_function <- fs_function(x, params[1], params[4], params[7], params[10]) +
-    fs_function(x, params[2], params[5], params[8], params[11]) +
-    fs_function(x, params[3], params[6], params[9], params[12])
+  output <- fs_function(x, h1, s1, p1, w1) +
+    fs_function(x, h2, s2, p2, w2) +
+    fs_function(x, h3, s3, p3, w3)
 
+  if (!is.null(h4) & !is.null(s4) & !is.null(p4) & !is.null(w4)) {
+    output <- output + fs_function(x, h4, s4, p4, w4)
+  }
+
+  output
 }
