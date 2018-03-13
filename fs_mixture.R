326ae88c727a70ae09fa3eb0af34a83518fdc443
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-13 17:17:12 +1100

    move old files out of R folder to test

---
+++ fs_mixture.R
@@ -0,0 +1,17 @@
+# Fraser-Suzuki three-part mixture
+#
+# This function combines three Fraser-Suzuki functions for the total mixture model.
+#
+# @param x temperature values
+# @param params parameter starting values
+# @return Fraser-Suzuki mixture model output
+# @keywords internal
+
+# combine the FS function three times for the three pseudo-components
+fs_mixture <- function (x, params) {
+
+  fs_mixture_function <- fs_function(x, params[1], params[4], params[7], params[10]) +
+    fs_function(x, params[2], params[5], params[8], params[11]) +
+    fs_function(x, params[3], params[6], params[9], params[12])
+
+}
---
+++ fs_model.R
@@ -0,0 +1,25 @@
+# Non-linear model using Fraser-Suzuki mixture model
+#
+# Non-linear model output using optimised parameter values
+# with a three-part mixture model using Fraser-Suzuki equation
+#
+# @param dataframe dataframe
+# @param params starting parameter values
+# @return model output
+# @keywords internal
+# @import minpack.lm
+
+# function to do the nls fit with the correct starting values
+fs_model <- function (dataframe, params, lb, ub) {
+
+  nlsLM(deriv ~ fs_mixture_wrap(temp_C, h1, h2, h3, s1, s2, s3, p1, p2, p3, w1, w2, w3),
+        start = list(h1 = params[1], h2 = params[2], h3 = params[3],
+                     s1 = params[4], s2 = params[5], s3 = params[6],
+                     p1 = params[7], p2 = params[8], p3 = params[9],
+                     w1 = params[10], w2 = params[11], w3 = params[12]),
+        data = dataframe,
+        control = nls.lm.control(maxiter = 1024, maxfev = 1e6),
+        lower = lb,
+        upper = ub)
+
+}
