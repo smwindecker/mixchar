52a55cab75beef19328982a8350ca1f6e735ec11
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-14 18:21:54 +1100

    revise since the list of parameters is in a different order

--- R/objective.R
+++ R/objective.R
@@ -20,10 +20,10 @@ objective <- function (theta, model, x, obs) {
 
   if (length(theta) == 16) {
     pred <- model(x,
-                  theta[1], theta[2], theta[3], theta[4],
                   theta[5], theta[6], theta[7], theta[8],
                   theta[9], theta[10], theta[11], theta[12],
-                  theta[13], theta[14], theta[15], theta[16])
+                  theta[13], theta[14], theta[15], theta[16],
+                  theta[1], theta[2], theta[3], theta[4])
   }
 
   # see how good it is
