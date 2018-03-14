b2d2cf16fbd3f132641559dd5104919340b39016
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-14 18:21:36 +1100

    revise so that the primary three curves have same designation

--- R/fs_model.R
+++ R/fs_model.R
@@ -26,12 +26,12 @@ fs_model <- function (dataframe, params, lb, ub) {
 
   if (length(params) == 16) {
 
-    frm <- deriv ~ fs_mixture(temp_C, h1, s1, p1, w1, h2, s2, p2, w2, h3, s3, p3, w3, h4, s4, p4, w4)
+    frm <- deriv ~ fs_mixture(temp_C, h1, s1, p1, w1, h2, s2, p2, w2, h3, s3, p3, w3, h0, s0, p0, w0)
 
-    start_list <- list(h1 = params[1], s1 = params[2], p1 = params[3], w1 = params[4],
-                       h2 = params[5], s2 = params[6], p2 = params[7], w2 = params[8],
-                       h3 = params[9], s3 = params[10], p3 = params[11], w3 = params[12],
-                       h4 = params[13], s4 = params[14], p4 = params[15], w4 = params[16])
+    start_list <- list(h0 = params[1], s0 = params[2], p0 = params[3], w0 = params[4],
+                       h1 = params[5], s1 = params[6], p1 = params[7], w1 = params[8],
+                       h2 = params[9], s2 = params[10], p2 = params[11], w2 = params[12],
+                       h3 = params[13], s3 = params[14], p3 = params[15], w3 = params[16])
 
   }
 
