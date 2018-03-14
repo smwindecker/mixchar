117dc115f3f5048e643420afd32e2fc5f1e339b3
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-14 14:12:12 +1100

    fix specifications to make first HC curve zero

--- R/fs_mixture.R
+++ R/fs_mixture.R
@@ -13,19 +13,19 @@
 #' @param s3 shape value for curve 3
 #' @param p3 position value for curve 3
 #' @param w3 width value for curve 3
-#' @param h4 height value for curve 4, if present
-#' @param s4 shape value for curve 4, if present
-#' @param p4 position value for curve 4, if present
-#' @param w4 width value for curve 4, if present
+#' @param h0 height value for curve 0, if present
+#' @param s0 shape value for curve 0, if present
+#' @param p0 position value for curve 0, if present
+#' @param w0 width value for curve 0, if present
 #' @return Fraser-Suzuki model output
 #'
 #' @export
 
 fs_mixture <- function (x,
-                        h0 = NULL, s0 = NULL, p0 = NULL, w0 = NULL,
                         h1, s1, p1, w1,
                         h2, s2, p2, w2,
-                        h3, s3, p3, w3) {
+                        h3, s3, p3, w3,
+                        h0 = NULL, s0 = NULL, p0 = NULL, w0 = NULL) {
 
   output <- fs_function(x, h1, s1, p1, w1) +
     fs_function(x, h2, s2, p2, w2) +
