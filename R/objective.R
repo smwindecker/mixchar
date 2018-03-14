448baac6492f580215379064406b34424470ed52
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-03-14 11:48:43 +1100

    revise so appropriate model format for the non wrapper mixture function

--- R/objective.R
+++ R/objective.R
@@ -11,7 +11,20 @@
 
 objective <- function (theta, model, x, obs) {
 
-  pred <- model(x, theta)
+  if (length(theta) == 12) {
+    pred <- model(x,
+                  theta[1], theta[2], theta[3], theta[4],
+                  theta[5], theta[6], theta[7], theta[8],
+                  theta[9], theta[10], theta[11], theta[12])
+  }
+
+  if (length(theta) == 16) {
+    pred <- model(x,
+                  theta[1], theta[2], theta[3], theta[4],
+                  theta[5], theta[6], theta[7], theta[8],
+                  theta[9], theta[10], theta[11], theta[12],
+                  theta[13], theta[14], theta[15], theta[16])
+  }
 
   # see how good it is
   target <- rmse(pred, obs)
