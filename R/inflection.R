ebdef4f4b491124124d36b5835510e31259431b3
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-04-23 22:07:37 +0200

    combine the two internal functions related to identifying the fourth peak

--- R/inflection.R
+++ R/inflection.R
@@ -1,9 +1,18 @@
-# Function to determine x-value of peak < 500 K
-#
-# @param x x-values
-# @param y y-values
-# @keywords internal
-# @import zoo
+#' Function to convert inflection point to logical argument for presence of fourth peak
+#'
+#' @param x numeric
+#' @keywords internal
+
+three_peaks <- function(x) {
+  return(length(x) == 0)
+}
+
+#' Function to determine x-value of peak < 500 K
+#'
+#' @param x x-values
+#' @param y y-values
+#' @keywords internal
+#' @import zoo
 ## script cred https://stats.stackexchange.com/questions/36309/how-do-i-find-peaks-in-a-dataset
 
 inflection <- function(x, y, w=1, ...) {
--- R/three_peaks.R
+++
@@ -1,8 +0,0 @@
-# Function to convert inflection point to logical argument for presence of fourth peak
-#
-# @param x numeric
-# @keywords internal
-
-three_peaks <- function(x) {
-  return(length(x) == 0)
-}
