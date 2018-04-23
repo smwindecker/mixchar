5f5e30555ab3bab1c2851607382f9f5948442b7b
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-04-23 21:16:28 +0200

    add function to get integration

---
+++ R/wt_component.R
@@ -0,0 +1,25 @@
+#' Calculate weight single component
+#'
+#' @param j component number
+#' @param fxn function to integrate over
+#' @return weight of component
+#'
+
+wt_component <- function (j, fxn, param_vec, lower_temp, upper_temp) {
+
+  # extract relevant parameter vector
+  names <- paste0(c("h", "s", "p", "w"), j)
+  idx <- match(names, names(param_vec))
+  param_sub <- param_vec[idx]
+
+  f_j <- function (x) {
+    fs_function(x, param_sub[1], param_sub[2], param_sub[3], param_sub[4])
+  }
+
+  # weight percent of each component, where the integral is the fraction
+  # of initial mass of that compoenent.
+  wt <- integrate(Vectorize(f_j), lower = lower_temp,
+                               upper = upper_temp)$value * 100
+
+  wt
+}
