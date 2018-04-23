3a1dc0d6cf597dda8c130e8a1a4787fcacc08e61
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-04-23 22:06:48 +0200

    revise roxygen with correct description

--- R/Bounds.R
+++ R/Bounds.R
@@ -1,15 +1,17 @@
 #' Accessor function to extract selected bounds
 #'
-#' @param object a process object
-#' @return $bounds of the object
-#' @keywords thermogravimetry fraser-suzuki deconvolution
+#' @param object the output of either the process or deconvolve functions
+#' @return Temperature bounds of the data in the object
+#' @keywords thermogravimetry fraser-suzuki deconvolution temperature
 #' @examples
 #' data(juncus)
-#' munge <- process(juncus, 'temp_C', 'mass_loss', 18.96, temp_type = 'C')
-#' Bounds(munge)
+#' tmp <- process(juncus, 'temp_C', 'mass_loss', 18.96, temp_type = 'C')
+#' Bounds(tmp)
 #'
 #' @export
 
 Bounds <- function (object) {
+
   object$bounds
+
 }
