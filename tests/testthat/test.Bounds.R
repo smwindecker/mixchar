76d7bf4573e9fdb31ce5901e7cc760a66d3a1f0f
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-05-09 09:56:24 +0200

    create test for Bounds function

---
+++ tests/testthat/test.Bounds.R
@@ -0,0 +1,10 @@
+context("Test for Bounds accessor function")
+
+test_that("function returns bounds", {
+
+  data(juncus)
+  tmp <- process(juncus, 'temp_C', 'mass_loss', 18.96)
+  output <- deconvolve(tmp, lower_temp = 130, upper_temp = 660)
+  expect_equal(Bounds(output), c(130, 660))
+
+})
