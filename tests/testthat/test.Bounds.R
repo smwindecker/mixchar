5ef8cbb0500f680bc04212348065fee22c124211
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-05-09 14:07:15 +0200

    modify test for bounds accessor so it actually does the code of the original

--- tests/testthat/test.Bounds.R
+++ tests/testthat/test.Bounds.R
@@ -5,6 +5,6 @@ test_that("function returns bounds", {
   data(juncus)
   tmp <- process(juncus, 'temp_C', 'mass_loss', 18.96)
   output <- deconvolve(tmp, lower_temp = 130, upper_temp = 660)
-  expect_equal(Bounds(output), c(130, 660))
+  expect_equal(Bounds(output), output$bounds)
 
 })
