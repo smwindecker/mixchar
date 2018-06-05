88a78bd15d0dca04aa118daf374db43e835551d6
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-06-05 13:41:02 +0200

    add tolerance

--- tests/testthat/test-fraser_suzuki.R
+++ tests/testthat/test-fraser_suzuki.R
@@ -3,7 +3,7 @@ context("Test for Fraser-Suzuki functions")
 test_that("function returns correct y value", {
 
   fxn_value <- fs_function(500, 10, .3, 600, 200)
-  expect_equal(fxn_value, 3.75394)
+  expect_equal(fxn_value, 3.75394, tolerance = 1e-3)
 
 })
 
@@ -16,6 +16,6 @@ test_that("mixture works properly", {
   expect_equal(fxn_four, fxn_value + 0.0001504429)
 
   expect_error(fs_mixture(500, .1, .3, 200, 100, .2, .25, 400, 60, .15, .26, 600, 200, .08, .22, 180),
-               'Specify correct number of parameters parameters')
+               'Specify correct number of parameters')
 
 })
