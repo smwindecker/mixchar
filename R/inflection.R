c2566291cbd81df8c71a69d77d08b4bf5f3a23b3
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-07-27 12:03:47 +1000

    specify function required from zoo

--- R/inflection.R
+++ R/inflection.R
@@ -3,8 +3,8 @@
 #' @param x numeric
 #' @keywords internal
 
-three_peaks <- function(x) {
-  return(length(x) == 0)
+three_peaks <- function (x) {
+  return (length(x) == 0)
 }
 
 #' Function to determine x-value of peak < 500 K
@@ -13,13 +13,13 @@ three_peaks <- function(x) {
 #' @param y y-values
 #' @keywords internal
 #' @return returns a list of values. $x returns the inflections points within x.
-#' @import zoo
+#' @importFrom zoo rollapply
 ## script cred https://stats.stackexchange.com/questions/36309/how-do-i-find-peaks-in-a-dataset
 
 inflection <- function(x, y, w = 1, ...) {
   n <- length(y)
   y.smooth <- loess(y ~ x, ...)$fitted
-  y.max <- rollapply(zoo(y.smooth), 2*w+1, max, align="center")
+  y.max <- zoo::rollapply(zoo(y.smooth), 2*w+1, max, align = "center")
   delta <- y.max - y.smooth[-c(1:w, n+1-1:w)]
   i.max <- which(delta <= 0) + w
   list(x = x[i.max], i = i.max, y.hat = y.smooth)
