b738f4b15a5df05f26d6b4a2b848f32fc8859018
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-07-27 12:05:39 +1000

    change argument names to be simpler

--- R/process.R
+++ R/process.R
@@ -1,11 +1,11 @@
-#' Processes Thermogravimetric Data
+#' Calculates the derivative rate of mass loss of thermogravimetric data
 #'
 #' This function processes thermogravimetric data by calculating
 #' the derivative of mass loss
 #'
 #' @param data dataframe
-#' @param temp_col column name containing temperature values
-#' @param massloss_col column name containing mass loss values in grams
+#' @param temp column name containing temperature values
+#' @param mass column name containing mass loss values in grams
 #' @param init_mass numeric value of initial sample mass in grams
 #' @param temp_type specify units of temperature, default = Celsius. Specify 'K' if in Kelvin
 #' @return decon list containing amended dataframe, bounds, model output, mass fractions
@@ -18,13 +18,21 @@
 #'
 #' @export
 
-process <- function (data, temp_col, massloss_col, init_mass,
-                        temp_type = 'C') {
+process <- function (data, temp, mass, init_mass,
+                     temp_type = 'C') {
 
-  if (temp_type == 'K') {
-    data$temp_C <- data[, temp_col] - 273
-  } else {
-    data$temp_C <- data[, temp_col]
+  temp_measures <- c('C', 'Celsius', 'K', 'Kelvin')
+
+  if (!is.TRUE(is.element(temp_type, temp_measures))) {
+    stop('Specify temperature either in Celsius or Kelvin')
+  }
+
+  if (temp_type == 'K' | temp_type == 'Kelvin') {
+    data$temp_C <- data[, temp] - 273
+  }
+
+  if (temp_type == 'C' | temp_type == 'Celsius') {
+    data$temp_C <- data[, temp]
   }
 
   if (data[1, 'temp_C']%%1!=0) {
@@ -35,7 +43,7 @@ process <- function (data, temp_col, massloss_col, init_mass,
   }
 
   # adjust mass loss given initial mass, use this for derivative
-  data_1$adj_massloss <- data_1[, massloss_col] / init_mass
+  data_1$adj_massloss <- data_1[, mass] / init_mass
 
   d <- -as.data.frame(diff(data_1$adj_massloss)/diff(data_1$temp_C))
   x <- rep(NA, ncol(d))
@@ -43,7 +51,7 @@ process <- function (data, temp_col, massloss_col, init_mass,
   colnames(deriv) <- 'deriv'
   data_2 <- cbind(data_1, deriv)
   data_2 <- data_2[-1,]
-  data_2$mass_T <- data_2[, massloss_col] + init_mass
+  data_2$mass_T <- data_2[, mass] + init_mass
 
   mod_data <- data_2[,c('temp_C', 'deriv', 'mass_T')]
 
