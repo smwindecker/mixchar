52ef9917785b0a977903461e459711aac3234404
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-07-27 15:43:14 +1000

    consolidate common scripts containing internal functions

--- R/inflection.R
+++ R/inflection.R
@@ -13,13 +13,13 @@ three_peaks <- function (x) {
 #' @param y y-values
 #' @keywords internal
 #' @return returns a list of values. $x returns the inflections points within x.
-#' @importFrom zoo rollapply
+#' @importFrom zoo rollapply zoo
 ## script cred https://stats.stackexchange.com/questions/36309/how-do-i-find-peaks-in-a-dataset
 
 inflection <- function(x, y, w = 1, ...) {
   n <- length(y)
   y.smooth <- loess(y ~ x, ...)$fitted
-  y.max <- zoo::rollapply(zoo(y.smooth), 2*w+1, max, align = "center")
+  y.max <- zoo::rollapply(zoo::zoo(y.smooth), 2*w+1, max, align = "center")
   delta <- y.max - y.smooth[-c(1:w, n+1-1:w)]
   i.max <- which(delta <= 0) + w
   list(x = x[i.max], i = i.max, y.hat = y.smooth)
--- R/weight_quantiles.R
+++ R/weight_quantiles.R
@@ -51,3 +51,69 @@ weight_quantiles <- function (output, seed) {
 
   list(weights = all_weights)
 }
+
+#' Calculate weight quantiles
+#'
+#' @param param_vec parameter estimates from minpack model
+#' @param output deconvolve output of model
+#' @importFrom stats coef deviance
+#' @return weights for each component
+#'
+
+get_weights <- function (param_vec, output) {
+
+  n_peaks <- output$n_peaks
+  lower_temp <- output$bounds[1]
+  upper_temp <- output$bounds[2]
+
+  if (n_peaks == 3) {
+    wt_percent <- c('HC' = NA, 'CL' = NA, 'LG' = NA)
+    curve_vec <- 1:3
+  }
+
+  if (n_peaks == 4) {
+    wt_percent <- c('HC_1' = NA, 'HC_2' = NA, 'CL' = NA, 'LG' = NA)
+    curve_vec <- 0:3
+  }
+
+  wt_percent <- sapply(curve_vec, wt_component,
+                       param_vec = param_vec,
+                       lower_temp = lower_temp,
+                       upper_temp = upper_temp)
+
+  if (length(wt_percent) == 3) names(wt_percent) <- c('HC', 'CL', 'LG')
+  if (length(wt_percent) == 4) names(wt_percent) <- c('HC_1', 'HC_2', 'CL', 'LG')
+
+  wt_percent
+}
+
+#' Calculate weight single component
+#'
+#' @param j component
+#' @param param_vec vector of parameters
+#' @param lower_temp lower temperature bound
+#' @param upper_temp upper temperature bound
+#' @importFrom glue glue
+#' @return weight of component
+#'
+
+wt_component <- function (j, param_vec, lower_temp, upper_temp) {
+
+  # extract relevant parameter vector
+  p <- c('height', 'skew', 'position', 'width')
+  names <- glue::glue('{p}_{j}')
+
+  idx <- match(names, names(param_vec))
+  param_sub <- param_vec[idx]
+
+  f_j <- function (x) {
+    fs_function(x, param_sub[1], param_sub[2], param_sub[3], param_sub[4])
+  }
+
+  # weight percent of each component, where the integral is the fraction
+  # of initial mass of that compoenent.
+  wt <- integrate(Vectorize(f_j), lower = lower_temp,
+                  upper = upper_temp)$value * 100
+
+  wt
+}
