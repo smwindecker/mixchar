1959726040ed6469fc3cdf1b35ac6d2f787ae0c8
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-08-15 14:10:03 +1000

    change wrapper from dontrun to donttest per cran requests

--- R/component_weights.R
+++ R/component_weights.R
@@ -4,7 +4,7 @@
 #' @return Extract mean fractions of the object
 #' @keywords thermogravimetry fraser-suzuki deconvolution
 #' @examples
-#' \dontrun{
+#' \donttest{
 #' data(juncus)
 #' tmp <- process(juncus, init_mass = 18.96,
 #'                temp = 'temp_C', mass_loss = 'mass_loss')
--- R/deconvolve.R
+++ R/deconvolve.R
@@ -17,7 +17,7 @@
 #' minpack.lm model fit, the number of curves fit, and estimated component weights
 #' @keywords thermogravimetry fraser-suzuki deconvolution
 #' @examples
-#' \dontrun{
+#' \donttest{
 #' data(juncus)
 #' tmp <- process(juncus, init_mass = 18.96,
 #'                temp = 'temp_C', mass_loss = 'mass_loss')
