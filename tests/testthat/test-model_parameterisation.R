72ccc77abd0af1908e40b0164e60dabfded8c753
Saras Windecker <windeckers@student.unimelb.edu.au>
2018-08-15 15:29:18 +1000

    add test for fs_model

--- tests/testthat/test-model_parameterisation.R
+++ tests/testthat/test-model_parameterisation.R
@@ -11,3 +11,30 @@ test_that("rmse returns correct value", {
   expect_equal(rmse(obs, pred), val)
 
 })
+
+test_that("model output correct", {
+
+  tmp <- process(juncus, init_mass = 18.96,
+                 temp = 'temp_C', mass_loss = 'mass_loss')
+
+  output <- deconvolve(tmp)
+  params <- model_parameters(output)
+  start_params <- c(height_1 = params[1,2], skew_1 = params[2,2],
+                    position_1 = params[3,2], width_1 = params[4,2],
+                    height_2 = params[5,2], skew_2 = params[6,2],
+                    position_2 = params[7,2], width_2 = params[8,2],
+                    height_3 = params[9,2], skew_3 = params[10,2],
+                    position_3 = params[11,2], width_3 = params[12,2])
+
+  lb <- c(0, -0.33, 0, 50,
+          0, -0.33, 290, 0,
+          0, -0.29, 330, 160)
+  ub <- c(2, 0.25, 280, 100,
+          2, 0.25, 380, 50,
+          2, 0.25, 430, 250)
+
+  fit <- fs_model(rate_data(tmp), start_params, lb, ub)
+
+  expect_equal(fs_model(rate_data(tmp), start_params, lb, ub), fit)
+
+})
